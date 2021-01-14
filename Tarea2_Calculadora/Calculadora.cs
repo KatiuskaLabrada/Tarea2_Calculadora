f44a515cedcc72253e7db47b95314feefaa5550f
katiuska Labrada <katiuska.labrada.evangelista13@outlook.com>
2021-01-13 21:39:32 -0400

    Add project files.

---
+++ Tarea2_Calculadora/Calculadora.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Programa que realiza las operaciones matemáticas basicas
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea2_Calculadora
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculadora_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
